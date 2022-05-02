78cb067ea15335e0f26390764803dfd44b0a13ee
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-05-02 10:02:18 +0530

    modify handTrackingModule.py

--- handTrackingModule.py
+++ handTrackingModule.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import math
 import mediapipe as mp
 
 
@@ -40,6 +41,40 @@ class handDetector():
                     cv2.circle(img, (cx, cy), 10, (255, 255, 255), cv2.FILLED)
 
         return PosList
+
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+ 
+        # Fingers
+        for id in range(1, 5):
+ 
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+ 
+        # totalFingers = fingers.count(1)
+ 
+        return fingers
+ 
+    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+ 
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+ 
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
 
 def main():
